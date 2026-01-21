568775b6d34694c593b7ab96071d30badfce64d5
Rangga Fermata <41527863+ranggafermata@users.noreply.github.com>
2026-01-21 08:08:53 +0700

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10494198
+    "zoneId": 10494206
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
